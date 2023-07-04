d0c9074d16f40870a0fc10322ea905b8b39e09bb
Aleksandra Banasiak <aleksandra.banasiak@protonmail.com>
2023-07-05 00:04:57 +0200

    Fix makefile

---
+++ boilerplate/data/ghostown-logo-02.c
@@ -0,0 +1,16 @@
+#define ghostown_logo_2_pal_count 8
+
+static const __data PaletteT ghostown_logo_2_pal = {
+  .count = 8,
+  .colors = {
+    0x653,
+    0xb83,
+    0xfb3,
+    0x987,
+    0xccb,
+    0xfff,
+    0x653,
+    0x653,
+  }
+};
+
